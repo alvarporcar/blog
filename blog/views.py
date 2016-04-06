cc94c420ae6737d018d4b84f273acef27d53195d
Alvar Porcar <alvar.porcar@gmail.com>
2016-04-06 19:54:19 +0200

    Segon commit ...

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,7 @@ from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from taggit.models import Tag
+from django.db.models import Count
 
 class PostListView(ListView):
 	queryset = Post.published.all()
@@ -20,7 +21,7 @@ def post_list(request, tag_slug=None):
 	if tag_slug:
 		tag = get_object_or_404(Tag, slug=tag_slug)
 		object_list = object_list.filter(tags__in=[tag])
-		
+
 	paginator = Paginator(object_list, 3) # 3 posts in each page
 	page = request.GET.get('page')
 	try:
@@ -31,7 +32,7 @@ def post_list(request, tag_slug=None):
 	except EmptyPage:
 		# If page is out of range deliver last page of results
 		posts = paginator.page(paginator.num_pages)
-	return render(request, 'blog/post/list.html', {'page':page, 'posts': posts})
+	return render(request, 'blog/post/list.html', {'page':page, 'posts': posts, 'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
@@ -52,8 +53,15 @@ def post_detail(request, year, month, day, post):
  	 		new_comment.save()
 	else:
  		comment_form = CommentForm()
-
-	return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'comment_form': comment_form})
+	# List of similar posts
+   	
+   	post_tags_ids = post.tags.values_list('id', flat=True)
+   	similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+                                 .exclude(id=post.id)
+   	similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+                               .order_by('-same_tags','-publish')[:4]
+	
+	return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'comment_form': comment_form, 'similar_posts':similar_posts})
 
  
 def post_share(request, post_id):
